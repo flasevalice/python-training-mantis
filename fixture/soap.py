f1d32702716527b4498683f73e6ab4dc7ab768b2
flasevalice <55132480+flasevalice@users.noreply.github.com>
2023-08-11 15:26:00 +0300

    Задание №26 base_url взят из конфига

--- fixture/soap.py
+++ fixture/soap.py
@@ -7,11 +7,12 @@ class SoapHelper:
 
     def __init__(self, app):
         self.app = app
-        self.client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
 
     def can_login(self, username, password):
+        base_url = self.app.config['web']['baseUrl']
+        client = Client(base_url + "mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
         try:
-            self.client.service.mc_login(username, password)
+            client.service.mc_login(username, password)
             return True
         except WebFault:
             return False
@@ -19,9 +20,11 @@ class SoapHelper:
     def get_projects_list(self):
         username = self.app.config['webadmin']['username']
         password = self.app.config['webadmin']['password']
+        base_url = self.app.config['web']['baseUrl']
+        client = Client(base_url + "mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
         try:
             # список проектов для пользователя
-            projects = self.client.service.mc_projects_get_user_accessible(username, password)
+            projects = client.service.mc_projects_get_user_accessible(username, password)
             project_list = []
             for project in projects:
                 project_list.append(Project(project_id=project.id,
